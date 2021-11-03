6e464a8184f8d7c6b12c460325c33d536a81e0b8
ariannaabdi362462 <44429267+ariannaabdi362462@users.noreply.github.com>
2021-11-03 13:59:42 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// You can require your own code as well...
+const funcs = require('./src/funcs.js')
+
+// Tell Express to serve HTML, JS, CSS etc from the public/ folder
+// See: http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'))
+
+// Our API routes will be here
+app.get('/api/login', function (req, res) {
+  // Return the response by calling our function
+res.send({});
+  //  res.send(funcs.myFunction());
+})
+
+// Tell us where we're running from
+console.log("Server running on http://localhost:" + port)
+app.listen(port)
